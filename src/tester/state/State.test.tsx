8458399c4b2986f8d1172fb3e6404f0b8082096c
ktcotz <naskret.kamil@gmail.com>
2024-08-03 23:45:56 +0200

    feat: state
    Stateless component testing. Learning more about userEvents.

--- src/tester/state/State.test.tsx
+++ src/tester/state/State.test.tsx
@@ -1,9 +1,52 @@
-import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 import { Counter } from "./State";
+import { userEvent } from "@testing-library/user-event";
 
 describe("Counter state tests", () => {
   test("Should render initially", () => {
     render(<Counter />);
+
+    const addButton = screen.getByRole("button", { name: "Dodaj" });
+    const subtractButton = screen.getByRole("button", { name: "Odejmij" });
+    const countElement = screen.getByRole("heading", { name: /Count/gi });
+
+    expect(addButton).toBeInTheDocument();
+    expect(subtractButton).toBeInTheDocument();
+    expect(countElement).toBeInTheDocument();
+  });
+
+  test("Should add to count", async () => {
+    render(<Counter />);
+    const addButton = screen.getByRole("button", { name: "Dodaj" });
+    const countElement = screen.getByRole("heading", { name: /Count/gi });
+
+    expect(countElement).toHaveTextContent("Count : 0");
+
+    await userEvent.click(addButton);
+
+    expect(countElement).toHaveTextContent("Count : 1");
+  });
+
+  test("Should remove count with disabled when 0", async () => {
+    render(<Counter />);
+    const subtractButton = screen.getByRole("button", { name: "Odejmij" });
+    const countElement = screen.getByRole("heading", { name: /Count/gi });
+    const addButton = screen.getByRole("button", { name: "Dodaj" });
+
+    expect(countElement).toHaveTextContent("Count : 0");
+
+    await userEvent.click(subtractButton);
+
+    expect(countElement).toHaveTextContent("Count : 0");
+
+    await userEvent.click(addButton);
+    await userEvent.click(addButton);
+
+    expect(countElement).toHaveTextContent("Count : 2");
+
+    await userEvent.click(subtractButton);
+
+    expect(countElement).toHaveTextContent("Count : 1");
   });
 });
--- src/tester/state/State.tsx
+++ src/tester/state/State.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hi!</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Dodaj</button>
+      <button
+        onClick={() => setCount((prev) => (prev === 0 ? prev : prev - 1))}
+      >
+        Odejmij
+      </button>
+      <h1>Count : {count}</h1>
     </div>
   );
 };
